2e966b04f2efc84dc45a7303d8ce6e64d6dbbcd5
Dalar <arttu.modig@gmail.com>
2015-02-25 22:23:11 +0200

    remove unnecessary stuff

--- src/handlers/upload_handlers.py
+++ src/handlers/upload_handlers.py
@@ -18,51 +18,9 @@ MAX_FILE_SIZE = 5000000  # bytes
 DOCUMENT_TYPES = 'application/pdf' #re.compile('image/(gif|p?jpeg|(x-)?png)')
 ACCEPT_FILE_TYPES = DOCUMENT_TYPES
 
-class S3PhotoUploadHandler(BaseHandler):
-    """ Ideally image resizing should be done with a queue and a seperate python process """
-    def get(self):
-        template_vars = {}
-        self.render('upload.html', **template_vars)
-
-    @tornado.web.asynchronous
-    def post(self):
-        file1 = self.request.files['file1'][0]
-        img = cStringIO.StringIO(file1['body'])
-        image = Image.open(img)
-
-        thread = threading.Thread(target=self.resize_the_image, args=(image,))
-        thread.start()
-
-    def resize_the_image(self, image):
-        im2 = image.resize((100, 100), Image.NEAREST)
-        out_im2 = cStringIO.StringIO()
-        im2.save(out_im2, 'PNG')
-        tornado.ioloop.IOLoop.instance().add_callback(functools.partial(self.upload_to_s3, out_im2))
-
-    def upload_to_s3(self, image2):
-        AWS_ACCESS_KEY_ID = ''
-        AWS_SECRET_ACCESS_KEY = ''
-
-        # credentials can be stored in environment AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY
-        conn = boto.connect_s3(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY)
-
-        # Connect to bucket and create key
-        b = conn.get_bucket('photos')
-        k = b.new_key('example5.png')
-
-        # Note we're setting contents from the in-memory string provided by cStringIO
-        k.set_contents_from_string(image2.getvalue(), headers={"Content-Type": "image/png"})
-        tornado.ioloop.IOLoop.instance().add_callback(functools.partial(self.image_uploaded))
-
-    def image_uploaded(self):
-        self.set_secure_cookie('flash', "File Uploaded")
-        self.redirect("/")
-
-
 class UploadHandler(BaseHandler):
     def get(self):
         self.render("upload-files.html")
-
 
 class PdfUploader(BaseHandler):
 
@@ -171,9 +129,3 @@ class PdfUploader(BaseHandler):
         print self.request.arguments
 
         self.finish()
-
-
-# class FileHandler(tornado.web.RequestHandler):
-#     file_body = self.request.files['filefieldname'][0]['body']
-#     img = Image.open(StringIO.StringIO(file_body))
-#     img.save("../img/", img.format)
