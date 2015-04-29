c921a357fb5ab3c1e92b432b73dc00232d10c174
Kalle O Ilves <kalleilv@mba-13.cs.helsinki.fi>
2015-04-29 10:00:54 +0300

    editing abstract and title

--- src/static/app/services/fileupload.js
+++ src/static/app/services/fileupload.js
@@ -1,33 +1,35 @@
 MatchingApp.service('Fileupload', function(){
   this.init = function(files, options){
+
     function scriptsLoaded(data) {
+        $('#fileupload').ready(function(){
+          var $form = $('#fileupload');
 
-        var $form = $('#fileupload')
+          $form.fileupload('option', 'done').call($form, $.Event('done'), { result: { files: files } })
 
-        $form.fileupload('option', 'done').call($form, $.Event('done'), { result: { files: files } })
+          $form.on('click', '.paper-title', function(e){
+            e.preventDefault();
 
-        $form.on('click', '.paper-title', function(e){
-          e.preventDefault();
+            var title = $(this).attr('data-title');
+            var key = $(this).attr('data-key');
 
-          var title = $(this).attr('data-title');
-          var key = $(this).attr('data-key');
-
-          options.fileClicked({
-            title: title,
-            key: key
+            options.fileClicked({
+              title: title,
+              key: key
+            });
           });
-        });
 
-        $form.bind('fileuploadsubmit', function (e, data) {
-            var inputs = data.context.find(':input');
-            if (inputs.filter(function () {
-                        return !this.value && $(this).prop('required');
-                    }).first().focus().length) {
-                data.context.find('button').prop('disabled', false);
-                return false;
-            }
+          $form.bind('fileuploadsubmit', function (e, data) {
+              var inputs = data.context.find(':input');
+              if (inputs.filter(function () {
+                          return !this.value && $(this).prop('required');
+                      }).first().focus().length) {
+                  data.context.find('button').prop('disabled', false);
+                  return false;
+              }
 
-            data.formData = inputs.serializeArray();
+              data.formData = inputs.serializeArray();
+          });
         });
     }
 
@@ -44,5 +46,13 @@ MatchingApp.service('Fileupload', function(){
     $(function () {
         $.get('static/template-upload.tmpl', uploadTemplateLoaded);
     });
+  };
+
+  this.updateFile = function(file){
+    var $fileEl = $('#fileupload').find('a[data-key=' + file.key + ']');
+
+    $fileEl.html(file.title);
+    $fileEl.removeAttr('data-title');
+    $fileEl.attr('data-title', file.title);
   };
 });
