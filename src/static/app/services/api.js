f37567f1071bd4fc21825f21f4aef546ba2c3894
Kalle O Ilves <kalleilv@mba-13.cs.helsinki.fi>
2015-08-17 10:28:44 +0300

    random results

--- src/static/app/services/api.js
+++ src/static/app/services/api.js
@@ -1,56 +1,4 @@
 MatchingApp.service('Api', function($http){
-  var _getTopics = function(){
-    var topics = [
-      {
-        weight: Math.round(Math.random()*100),
-        selected: false
-      },
-      {
-        weight: Math.round(Math.random()*100),
-        selected: false
-      },
-      {
-        weight: Math.round(Math.random()*100),
-        selected: false
-      },
-      {
-        weight: Math.round(Math.random()*100),
-        selected: false
-      },
-      {
-        weight: Math.round(Math.random()*100),
-        selected: false
-      },
-      {
-        weight: Math.round(Math.random()*100),
-        selected: false
-      }
-    ];
-
-    topics.forEach(function(topic){
-      topic.relatedArticles = [];
-      topic.keywords = [];
-
-      _.times(10, function(){
-        topic.keywords.push({
-          content: 'lorem',
-          weight: Math.random()
-        });
-      });
-
-      _.times(5, function(){
-        topic.relatedArticles.push({
-          title: 'Lorem ipsum dolor sit amet',
-          authors: 'Lorem ipsum, Dolor sit',
-          abstract: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sodales ex eget nibh facilisis, eget condimentum neque sollicitudin. Sed rutrum tempus malesuada. Donec laoreet nec sapien quis dictum. Nulla ultrices pretium lorem, sit amet malesuada magna sagittis nec. Nullam id purus posuere, pretium quam non, pulvinar elit. Suspendisse purus elit, sodales et magna sit amet, molestie tempus turpis. Phasellus ut accumsan purus.',
-          weight: Math.random()
-        });
-      });
-    });
-
-    return topics;
-  }
-
   this.updateBio = function(newBio){
     return $http.post('/update_bio', { bio_new_text: newBio });
   }
@@ -76,8 +24,8 @@ MatchingApp.service('Api', function($http){
   }
 
   this.topics = function(options){
-    var encodedKeyword = encodeURIComponent(options.keyword);
 
-    return $http.get('/topics');
+
+    return $http.get('/topicsearch/' + options.keyword.replace(/ /g, '').toLowerCase());
   }
 });
